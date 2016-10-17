967a5edd447b5145cd236768ee71c41ca456a3af
shamay <shamay@laptop>
2016-10-17 16:01:22 -0400

    changed CM_ERR in navigation

--- lab4/src/lab4localization/Navigation.java
+++ lab4/src/lab4localization/Navigation.java
@@ -14,7 +14,7 @@ import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 public class Navigation {
 	final static int FAST = 200, SLOW = 50, ACCELERATION = 4000;
-	final static double DEG_ERR = 5.0, CM_ERR = 1.0;
+	final static double DEG_ERR = 5.0, CM_ERR = 2.0;
 	private Odometer odometer;
 	private EV3LargeRegulatedMotor leftMotor, rightMotor;
 
